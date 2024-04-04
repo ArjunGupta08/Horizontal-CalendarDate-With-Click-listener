ada42e84ce9e7f7df34ff819e114b405bc37880e
Arjun Gupta <arjungupta0817@gmail.com>
2024-04-04 23:45:56 +0530

    CalendarSetUp

--- app/src/main/java/com/arjun/horizontalcalendardate/CalendarSetUp.kt
+++ app/src/main/java/com/arjun/horizontalcalendardate/CalendarSetUp.kt
@@ -1,22 +1,18 @@
 package com.arjun.horizontalcalendardate
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.content.Context
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SnapHelper
 import java.text.SimpleDateFormat
-import java.util.*
-import kotlin.collections.ArrayList
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
-class MainActivity : AppCompatActivity(), CalendarAdapter.onItemClickListener {
-
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var tvDateMonth: TextView
-    private lateinit var ivCalendarNext: ImageView
-    private lateinit var ivCalendarPrevious: ImageView
+class CalendarSetUp() {
 
     private val sdf = SimpleDateFormat("MMMM yyyy", Locale.ENGLISH)
     private val cal = Calendar.getInstance(Locale.ENGLISH)
@@ -25,50 +21,28 @@ class MainActivity : AppCompatActivity(), CalendarAdapter.onItemClickListener {
     private lateinit var adapter: CalendarAdapter
     private val calendarList2 = ArrayList<CalendarDateModel>()
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-        tvDateMonth = findViewById(R.id.text_date_month)
-        recyclerView = findViewById(R.id.recyclerView)
-        ivCalendarNext = findViewById(R.id.iv_calendar_next)
-        ivCalendarPrevious = findViewById(R.id.iv_calendar_previous)
-        val calendarSetUp = CalendarSetUp()
-
-        calendarSetUp.setUpCalendarAdapter(recyclerView, this@MainActivity)
-        calendarSetUp.setUpCalendarClickListener(ivCalendarNext, ivCalendarPrevious, this@MainActivity)
-
-    }
-    override fun onItemClick(text: String, date: String, day: String) {
-        findViewById<TextView>(R.id.selectedDate).text = "Selected date: $text"
-        findViewById<TextView>(R.id.selectedDD).text = "Selected DD: $date"
-        findViewById<TextView>(R.id.selectedDay).text = "Selected day: $day"
-    }
-    /**
+    /*
      * Set up click listener
      */
-
-    private fun setUpClickListener() {
+    fun setUpCalendarClickListener(ivCalendarNext: ImageView, ivCalendarPrevious: ImageView, listener: CalendarAdapter.onItemClickListener) {
         ivCalendarNext.setOnClickListener {
             cal.add(Calendar.MONTH, 1)
-            setUpCalendar()
+            setUpCalendar(listener)
         }
         ivCalendarPrevious.setOnClickListener {
             cal.add(Calendar.MONTH, -1)
             if (cal == currentDate)
-                setUpCalendar()
+                setUpCalendar(listener)
             else
-                setUpCalendar()
+                setUpCalendar(listener)
         }
     }
 
 
-    /**
+    /*
      * Setting up adapter for recyclerview
      */
-    private fun setUpAdapter() {
+    fun setUpCalendarAdapter(recyclerView: RecyclerView, listener : CalendarAdapter.onItemClickListener) {
         val snapHelper: SnapHelper = LinearSnapHelper()
         snapHelper.attachToRecyclerView(recyclerView)
         adapter = CalendarAdapter { calendarDateModel: CalendarDateModel, position: Int ->
@@ -76,17 +50,19 @@ class MainActivity : AppCompatActivity(), CalendarAdapter.onItemClickListener {
                 calendarModel.isSelected = index == position
             }
             adapter.setData(calendarList2)
-            adapter.setOnItemClickListener(this@MainActivity)
+            adapter.setOnItemClickListener(listener)
         }
         recyclerView.adapter = adapter
+
+        setUpCalendar(listener)
     }
 
-    /**
+    /*
      * Function to setup calendar for every month
      */
-    private fun setUpCalendar() {
+    private fun setUpCalendar(listener: CalendarAdapter.onItemClickListener) {
         val calendarList = ArrayList<CalendarDateModel>()
-        tvDateMonth.text = sdf.format(cal.time)
+//        tvDateMonth.text = sdf.format(cal.time)
         val monthCalendar = cal.clone() as Calendar
         val maxDaysInMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH)
         dates.clear()
@@ -98,7 +74,7 @@ class MainActivity : AppCompatActivity(), CalendarAdapter.onItemClickListener {
         }
         calendarList2.clear()
         calendarList2.addAll(calendarList)
-        adapter.setOnItemClickListener(this@MainActivity)
+        adapter.setOnItemClickListener(listener)
         adapter.setData(calendarList)
     }
 
